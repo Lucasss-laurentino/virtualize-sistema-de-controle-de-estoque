e0c5a2e66d05deaa7e33a139c396543786cb8d6e
Lucasss-laurentino <lucaselorenzohenrique@gmail.com>
2023-10-04 18:10:35 -0300

    Excluindo arquivos desnecessarios, configurando react-router-dom

---
+++ controle-de-estoque/src/routes.js
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+export default appRoute = () => {
+    
+    return(
+        <Router>
+            <Routes>
+                <Route />
+            </Routes>
+        </Router>
+    );
+
+}
